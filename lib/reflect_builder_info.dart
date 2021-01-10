ef57803b394a3d9e5a5af7339d4183b2a0c3af13
Nils ten Hoeve <nilstenhoeve@gmail.com>
2021-01-10 16:36:21 +0100

    Fixed #40

--- lib/reflect_builder_info.dart
+++ lib/reflect_builder_info.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
 import 'package:reflect_framework/reflect_info_json.dart';
 
 import 'reflect_info_application.dart';
@@ -12,18 +14,44 @@ import 'reflect_info_application.dart';
 /// - create dart files
 
 main() async {
-  String jsonString = await File('C:/Users/nilsth/AndroidStudioProjects/flutter-reflect-framework/.dart_tool/build/generated/reflect_framework/lib/reflect_info.combined.json').readAsString();
+  String jsonString = await File(
+          'C:/Users/nilsth/AndroidStudioProjects/flutter-reflect-framework/.dart_tool/build/generated/reflect_framework/lib/reflect_info.combined.json')
+      .readAsString();
   var json = jsonDecode(jsonString);
-  _createDartCode(json);
+  _createReflectGeneratedLibFile(json);
 }
 
-_createDartCode(json) async {
-  ReflectJson reflectInfo = await _readReflectInfoJsonFile(json);
-  ApplicationInfoCode(reflectInfo);
-  //TODO add dart codes in a Library and store it in source
+const reflectGeneratedPath = 'lib/reflect_generated.dart';
+
+void _createReflectGeneratedLibFile(json) {
+  File file = File(reflectGeneratedPath);
+  String dartCode = _createReflectGeneratedLibCode(json);
+  file.writeAsString(dartCode);
 }
 
-Future<ReflectJson> _readReflectInfoJsonFile(json) async {
+String _createReflectGeneratedLibCode(json)  {
+  Library reflectGeneratedLib =  _createReflectGeneratedLib(json);
+  final emitter = DartEmitter(Allocator.simplePrefixing());
+  String dartCode =  DartFormatter().format('${reflectGeneratedLib.accept(emitter)}');
+  return dartCode;
+}
+
+Library _createReflectGeneratedLib(json)  {
+  ReflectJson reflectJson =  _readReflectInfoJsonFile(json);
+  final generatedLib = Library(
+      (b) => b.body.addAll([ApplicationInfoCodeFactory(reflectJson).create()]));
+  return generatedLib;
+  // ApplicationInfoCode applicationInfoCode=ApplicationInfoCode(reflectInfo);
+  // List<ExecutableInfo> preProcessors=reflectInfo.findActionMethodPreProcessorFunctions();
+  // List<ExecutableInfo> processors=reflectInfo.findActionMethodProcessorFunctions();
+  // List<ClassInfo> serviceClasses= reflectInfo.findPotentialServiceClasses( preProcessors,processors);
+  // ServiceClassesInfoCode serviceClassesInfoCode=ServiceClassesInfoCode(reflectInfo);
+  // List<ServiceClassInfoCode> serviceClassInfoCodes=serviceClasses.map((s) => ServiceClassInfoCode(s));
+  // List<ClassInfo> domainClasses= reflectInfo.findPotentialDomainClasses(serviceClasses);
+  // List<DomainClassInfoCode> domainClassCodes=serviceClasses.map((s) => DomainClassInfoCode(s));
+}
+
+ReflectJson _readReflectInfoJsonFile(json)  {
   return ReflectJson.fromJson(json);
 }
 
@@ -31,32 +59,23 @@ Future<ReflectJson> _readReflectInfoJsonFile(json) async {
 class ReflectInfoBuilder implements Builder {
   @override
   Map<String, List<String>> get buildExtensions => {
-        '.combined.json': [ReflectJson.libraryExtension]
+        '.combined.json': ['/../reflect_generated.dart']
       };
 
   @override
+
+
+  @override
   FutureOr<void> build(BuildStep buildStep) async {
-    String jsonString = await buildStep.readAsString(buildStep.inputId);
-    var json=jsonDecode(jsonString);
-    _createDartCode(json);
-    // ApplicationInfoCode applicationInfoCode=ApplicationInfoCode(reflectInfo);
-
-    // List<ExecutableInfo> preProcessors=reflectInfo.findActionMethodPreProcessorFunctions();
-    //
-    // List<ExecutableInfo> processors=reflectInfo.findActionMethodProcessorFunctions();
-    //
-    // List<ClassInfo> serviceClasses= reflectInfo.findPotentialServiceClasses( preProcessors,processors);
-    //
-    // ServiceClassesInfoCode serviceClassesInfoCode=ServiceClassesInfoCode(reflectInfo);
-    //
-    // List<ServiceClassInfoCode> serviceClassInfoCodes=serviceClasses.map((s) => ServiceClassInfoCode(s));
-    //
-    // List<ClassInfo> domainClasses= reflectInfo.findPotentialDomainClasses(serviceClasses);
-    //
-    // List<DomainClassInfoCode> domainClassCodes=serviceClasses.map((s) => DomainClassInfoCode(s));
-    //
-    //
-
-    //TODO write dart code files;
+    try {
+      String jsonString = await buildStep.readAsString(buildStep.inputId);
+      var json = jsonDecode(jsonString);
+      String dartCode = _createReflectGeneratedLibCode(json);
+      AssetId destination =
+          AssetId(buildStep.inputId.package, reflectGeneratedPath);
+      buildStep.writeAsString(destination, dartCode);
+    } catch (exception, stacktrace) {
+      print('$exception\n$stacktrace');
+    }
   }
 }
